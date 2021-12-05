f9bfa61bb540e8df04754e7be3dd6dcf29105c40
kinson <swilkinsonhunter@gmail.com>
2021-12-04 22:32:47 -0700

    Update to runtime.exs

---
+++ lnk_platform/config/runtime.exs
@@ -0,0 +1,20 @@
+import Config
+
+config :lnk_platform, LnkPlatformWeb.Endpoint, server: true
+
+# Configure your database
+config :lnk_platform, LnkPlatform.Repo, database: "priv/database.db"
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :lnk_platform, LnkPlatformWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4201"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  secret_key_base: secret_key_base
